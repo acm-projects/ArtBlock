5413ab650f69b9112e6deac4414f5ff7fd666e53
tarzerk <its.krei@gmail.com>
2021-03-08 21:34:27 -0600

    Add files for each screen and nav widget

---
+++ client_artblock/lib/nav.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Nav extends StatefulWidget {
+  @override
+  _NavState createState() => _NavState();
+}
+
+class _NavState extends State<Nav> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
