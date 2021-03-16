2613554a0ec923c51c6bf1736fc36f71a8ed97d4
tarzerk <its.krei@gmail.com>
2021-03-16 14:11:17 -0500

    add search bar for the homescreen

--- client_artblock/lib/home-screen.dart
+++ client_artblock/lib/home-screen.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+  TextEditingController searchController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text('home tab')),
+    return Scaffold(
+      body: Center(
+        child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 40.0),
+            child: Card(
+                elevation: 3.0,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0)),
+                child: TextField(
+                  controller: searchController,
+                  decoration: InputDecoration(
+                      hintText: "Search",
+                      contentPadding: EdgeInsets.symmetric(
+                          horizontal: 32.0, vertical: 14.0),
+                      suffixIcon: Icon(Icons.search),
+                      border: InputBorder.none),
+                ))),
+      ),
     );
   }
 }
