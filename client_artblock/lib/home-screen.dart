f8a1d2e437e65b3cf2d84416fadc48f2cdb75a0b
tarzerk <its.krei@gmail.com>
2021-03-16 22:48:45 -0500

    added buttons to all homescreen features

--- client_artblock/lib/home-screen.dart
+++ client_artblock/lib/home-screen.dart
@@ -6,22 +6,71 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 40.0),
-            child: Card(
-                elevation: 3.0,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0)),
-                child: TextField(
-                  controller: searchController,
-                  decoration: InputDecoration(
-                      hintText: "Search",
-                      contentPadding: EdgeInsets.symmetric(
-                          horizontal: 32.0, vertical: 14.0),
-                      suffixIcon: Icon(Icons.search),
-                      border: InputBorder.none),
-                ))),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(height: 250),
+          RichText(
+            text: TextSpan(
+              style: TextStyle(
+                // global text style
+                fontSize: 45,
+              ),
+              children: <TextSpan>[
+                TextSpan(
+                    text: 'Art',
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold)),
+                TextSpan(
+                    text: 'Block',
+                    style: TextStyle(
+                        color: Colors.red, fontWeight: FontWeight.bold)),
+              ],
+            ),
+          ),
+          SizedBox(height: 30),
+          Container(
+              padding: EdgeInsets.symmetric(horizontal: 40.0),
+              child: Card(
+                  elevation: 3.0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0)),
+                  child: TextField(
+                    controller: searchController,
+                    decoration: InputDecoration(
+                        hintText: "Search",
+                        contentPadding: EdgeInsets.symmetric(
+                            horizontal: 32.0, vertical: 14.0),
+                        suffixIcon: Icon(Icons.search),
+                        border: InputBorder.none),
+                  ))),
+          SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Ink(
+                decoration: ShapeDecoration(
+                  shape: CircleBorder(), // I want to add a border to the button
+                ),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.camera_alt_rounded),
+                  color: Colors.black,
+                ),
+              ),
+              Ink(
+                decoration: ShapeDecoration(
+                  shape: CircleBorder(), // I want to add a border to the button
+                ),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.pool_sharp),
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
