59be42ea6053e4ba3742a1af3845ca154a7e36fb
TwentyPorts <2rayxu+github@gmail.com>
2021-03-21 19:20:29 -0500

    add harvard art museum api and filter by color, hexcode + keyword

--- server/quickstart/views.py
+++ server/quickstart/views.py
@@ -1,3 +1,28 @@
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 from django.shortcuts import render
+from .services import get_colors, get_keywords
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("loaded")
+
+
+class GetColors(TemplateView):
+    template_name = 'colors.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = {
+            'colors': get_colors(kwargs['color']),
+        }
+        return context
+
+
+class GetKeywords(TemplateView):
+    template_name = 'colors.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = {
+            'colors': get_keywords(kwargs['keyword']),
+        }
+        return context
