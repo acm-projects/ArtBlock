e68232af0a4b36c9456ce31224dee39558e953a0
tarzerk <its.krei@gmail.com>
2021-04-11 15:45:39 -0500

    Add colors page and routes

---
+++ client_artblock/lib/color-picker-screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter_hsvcolor_picker/flutter_hsvcolor_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'home-screen.dart';
+
+class _ColorPageState extends State<ColorPage> {
+  Color _customColor = Colors.blue;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          AppBar(
+            title: Text(
+              'ArtBlock',
+              style: GoogleFonts.poppins(fontSize: 20),
+            ),
+            elevation: 4.0,
+            leading: IconButton(
+                icon: Icon(Icons.arrow_back_ios_sharp),
+                onPressed: () => {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Home()))
+                    }),
+            actions: [IconButton(icon: Icon(Icons.check), onPressed: () => {})],
+          ),
+          SizedBox(height: 15),
+          Container(
+            width: 350,
+            height: 100,
+            child: Center(
+              child: Text(
+                'My current color is: ',
+                style: GoogleFonts.poppins(fontSize: 15),
+              ),
+            ),
+            color: _customColor,
+          ),
+          Card(
+              elevation: 4.0,
+              margin: EdgeInsets.all(30.0),
+              child: new ColorPicker(
+                color: Colors.blue,
+                onChanged: (value) {
+                  setState(() {
+                    _customColor = value;
+                  });
+                },
+              ))
+        ],
+      ),
+    );
+  }
+}
+
+class ColorPage extends StatefulWidget {
+  @override
+  _ColorPageState createState() => _ColorPageState();
+}
