733fada25f10114e2e1ac61dd4468395bb06611e
TwentyPorts <2rayxu+github@gmail.com>
2021-04-23 19:10:52 -0500

    small changes

--- client_artblock/lib/camera-feature.dart
+++ client_artblock/lib/camera-feature.dart
@@ -7,7 +7,7 @@ void main() {
   runApp(new MaterialApp(
     title: "Camera",
     home: CameraScreen(),
-        ));
+  ));
 }
 
 class CameraScreen extends StatefulWidget {
@@ -16,80 +16,85 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
-  
   File imageFile;
   // upload from gallery method
   _openGallery(BuildContext context) async {
     var picture = await ImagePicker.pickImage(source: ImageSource.gallery);
-    this.setState((){
+    this.setState(() {
       imageFile = picture;
     });
     Navigator.of(context).pop();
   }
+
   // take a picture method
   _openCamera(BuildContext context) async {
     var picture = await ImagePicker.pickImage(source: ImageSource.camera);
-    this.setState((){
+    this.setState(() {
       imageFile = picture;
     });
     Navigator.of(context).pop();
   }
-  
-  Future<void> _showChoiceDialog(BuildContext context){
-    return showDialog(context: context,builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text("Choose An Option"),
-        
-        content: SingleChildScrollView(
-          child: ListBody(
-            children: <Widget>[
-              GestureDetector(
-                child: Text("Gallery"),onTap: (){
-                  _openGallery(context);
-                },
+
+  Future<void> _showChoiceDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Choose An Option"),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  GestureDetector(
+                    child: Text("Gallery"),
+                    onTap: () {
+                      _openGallery(context);
+                    },
+                  ),
+                  Padding(padding: EdgeInsets.all(8.0)),
+                  GestureDetector(
+                    child: Text("Camera"),
+                    onTap: () {
+                      _openCamera(context);
+                    },
+                  )
+                ],
               ),
-              Padding(padding: EdgeInsets.all(8.0)),
-              GestureDetector(
-                child: Text("Camera"),onTap: (){
-                  _openCamera(context);
-                },
-              )
-            ],
-          ),
-        ),
-      );
-    });
+            ),
+          );
+        });
   }
-  
+
   // shows selected image or tells the user that no image is selected
-  Widget _deicdeImageView(){
-    if(imageFile == null){
+  Widget _deicdeImageView() {
+    if (imageFile == null) {
       return Text("No Image Selected");
     } else {
-      return Image.file(imageFile,width: 400,height: 400);
+      return Image.file(imageFile, width: 400, height: 400);
     }
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Camera Screen"),
-        ),
-       body: Container(
+      ),
+      body: Container(
         child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            _deicdeImageView();
-            ElevatedButton(onPressed: (){
-              _showChoiceDialog(context);
-            },child: Text("Select Image"),)
-          ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              _deicdeImageView(),
+              ElevatedButton(
+                onPressed: () {
+                  _showChoiceDialog(context);
+                },
+                child: Text("Select Image"),
+              )
+            ],
+          ),
         ),
-        ),
-       ),
+      ),
     );
   }
 }
-
